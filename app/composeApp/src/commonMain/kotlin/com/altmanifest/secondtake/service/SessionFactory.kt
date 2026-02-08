e34afa56022e16e23b753f95265f50aa7d3f41cc
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-08 19:36:20 +0100

    refactor: add setup parameter

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/service/SessionFactory.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/service/SessionFactory.kt
@@ -1,6 +1,7 @@
 package com.altmanifest.secondtake.service
 
 import com.altmanifest.secondtake.application.SessionFactory
+import com.altmanifest.secondtake.application.SessionFactory.Setup
 import com.altmanifest.secondtake.domain.Round
 import com.altmanifest.secondtake.domain.Session
 import com.altmanifest.secondtake.domain.Title
@@ -9,8 +10,12 @@ class SessionFactory(
     private val titleProvider: TitleProvider,
     private val roundFactory: RoundFactory
 ) : SessionFactory {
-    override fun create(): SessionFactory.CreateResult =
-        when (val roundResult = roundFactory.create(titleProvider.getAll())) {
+    override fun create(setup: Setup): SessionFactory.CreateResult = when (setup) {
+        is Setup.Default -> create { titleProvider.getAll() }
+    }
+
+    fun create(provideTitles: () -> Set<Title>): SessionFactory.CreateResult =
+        when (val roundResult = roundFactory.create(titles = provideTitles())) {
             is RoundFactory.CreateResult.NoComparisons -> SessionFactory.CreateResult.NoComparisons
             is RoundFactory.CreateResult.NoTitles -> SessionFactory.CreateResult.NoTitles
             is RoundFactory.CreateResult.Success -> SessionFactory.CreateResult.Success(
--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/service/TitleProvider.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/service/TitleProvider.kt
@@ -1,9 +1,14 @@
 package com.altmanifest.secondtake.service
 
+import com.altmanifest.secondtake.domain.Genre
 import com.altmanifest.secondtake.domain.Title
 
 class TitleProvider {
     fun getAll() : Set<Title> {
+        TODO()
+    }
+
+    fun getByGenre(genre: Genre) : Set<Title> {
         TODO()
     }
 }
