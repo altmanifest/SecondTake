f446e9afc21fe80b5e433aa13af4d7725264ab14
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-08 15:31:50 +0100

    feat: requeue skipped tittles

---
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/service/RoundFactory.kt
@@ -0,0 +1,27 @@
+package com.altmanifest.secondtake.service
+
+import com.altmanifest.secondtake.domain.Comparison
+import com.altmanifest.secondtake.domain.ComparisonSchedule
+import com.altmanifest.secondtake.domain.ComparisonSchedule.Companion.scheduleForComparison
+import com.altmanifest.secondtake.domain.Round
+import com.altmanifest.secondtake.domain.Round.Companion.createRound
+import com.altmanifest.secondtake.domain.Title
+
+class RoundFactory(private val comparisonConfig: Comparison.Config, private val capacity: Round.Capacity) {
+    fun create(titles: Set<Title>, exclude: Set<Pair<Title, Title>> = setOf()): CreateResult =
+        when (val schedule = titles.scheduleForComparison(comparisonConfig, exclude)) {
+            is ComparisonSchedule.CreateResult.NoTitles -> CreateResult.NoTitles
+            is ComparisonSchedule.CreateResult.NoComparisons ->
+                CreateResult.NoComparisons
+
+            is ComparisonSchedule.CreateResult.Success ->
+                CreateResult.Success(schedule.value.createRound(capacity))
+        }
+
+    sealed class CreateResult {
+        data class Success(val roundResult: Round.CreateResult) : CreateResult()
+        object NoTitles : CreateResult()
+        object NoComparisons : CreateResult()
+    }
+}
+
