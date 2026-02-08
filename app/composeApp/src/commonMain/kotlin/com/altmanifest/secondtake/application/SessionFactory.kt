e22ec42651f032da79e941ae7da3498d655e3206
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-08 19:36:20 +0100

    refactor: add setup parameter

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/SessionFactory.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/SessionFactory.kt
@@ -3,11 +3,16 @@ package com.altmanifest.secondtake.application
 import com.altmanifest.secondtake.domain.Session
 
 interface SessionFactory {
-    fun create(): CreateResult
+    fun create(setup: Setup = Setup.Default): CreateResult
 
     sealed class CreateResult {
         data class Success(val session: Session) : CreateResult()
         object NoTitles : CreateResult()
         object NoComparisons : CreateResult()
     }
+
+    sealed class Setup {
+        object Default : Setup()
+    }
 }
+
