51067ddd6e7a83f2ce577c8b00598465b55cd0cc
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-09 03:12:57 +0100

    feat: remove newly rated from forgotten

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/ForgottenTitlesProvider.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/ForgottenTitlesProvider.kt
@@ -3,5 +3,5 @@ package com.altmanifest.secondtake.application
 import com.altmanifest.secondtake.domain.Title
 
 interface ForgottenTitlesProvider {
-    fun isForgotten(title: Title): Boolean
+    fun get(id: Title.Id): Title?
 }
--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/domain/Title.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/domain/Title.kt
@@ -1,3 +1,5 @@
 package com.altmanifest.secondtake.domain
 
-data class Title(val value: String, val posterUrl: String, val genre: Genre, val rating: Rating)
+data class Title(val id: Id, val value: String, val posterUrl: String, val genre: Genre, val rating: Rating) {
+    value class Id(val value: String)
+}
