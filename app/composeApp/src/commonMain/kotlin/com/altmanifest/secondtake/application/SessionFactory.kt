59500afd91c1d7a44d932cc9a96ba080fcb9eea6
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-09 02:59:09 +0100

    refactor: move title retrieval to the use case

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/SessionFactory.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/application/SessionFactory.kt
@@ -1,20 +1,15 @@
 package com.altmanifest.secondtake.application
 
-import com.altmanifest.secondtake.domain.Genre
 import com.altmanifest.secondtake.domain.Session
+import com.altmanifest.secondtake.domain.Title
 
 interface SessionFactory {
-    fun create(setup: Setup = Setup.Default): CreateResult
+    fun create(titles: Set<Title>): CreateResult
 
     sealed class CreateResult {
         data class Success(val session: Session) : CreateResult()
         object NoTitles : CreateResult()
         object NoComparisons : CreateResult()
-    }
-
-    sealed class Setup {
-        object Default : Setup()
-        data class ByGenre(val genre: Genre) : Setup()
     }
 }
 
