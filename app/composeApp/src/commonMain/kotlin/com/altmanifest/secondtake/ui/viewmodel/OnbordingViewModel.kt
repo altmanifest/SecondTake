27c0f733a7c4c4e9f1b6932469086f2aef5dd344
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-17 15:38:40 +0100

    feat: load connected providers

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/ui/viewmodel/OnbordingViewModel.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/ui/viewmodel/OnbordingViewModel.kt
@@ -4,29 +4,39 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.altmanifest.secondtake.ui.DefinedProvider
 import com.altmanifest.secondtake.ui.Provider
+import kotlinx.coroutines.launch
 
 data class OnboardingUiState(
     val providers: List<AvailableProvider> = listOf()
 )
 
-class OnboardingViewmodel : ViewModel() {
-    private val providers = mapOf(
-        DefinedProvider.IMDB to AvailableProvider.Planned(DefinedProvider.IMDB),
-        DefinedProvider.FILMWEB to AvailableProvider.Planned(DefinedProvider.FILMWEB),
-        DefinedProvider.ONLYFILMS to AvailableProvider.Planned(DefinedProvider.ONLYFILMS),
-        DefinedProvider.MOCKIFY to AvailableProvider.Disconnected(DefinedProvider.MOCKIFY) {}
-    )
+class OnboardingViewmodel(val source: ConnectedProviderSource) : ViewModel() {
+    private val providers = listOf(
+        AvailableProvider.Planned(DefinedProvider.IMDB),
+        AvailableProvider.Planned(DefinedProvider.FILMWEB),
+        AvailableProvider.Planned(DefinedProvider.ONLYFILMS),
+        AvailableProvider.Disconnected(DefinedProvider.MOCKIFY)
+        )
 
     var uiState by mutableStateOf(OnboardingUiState())
         private set
 
 
     init {
-        uiState = OnboardingUiState(
-            providers = providers.values.toList()
-        )
+        viewModelScope.launch {
+            loadProviders()
+        }
+    }
+
+    suspend fun loadProviders() {
+        val providers = source.getAll()
+        val existingIds = providers.map { it.id }
+        val disconnectedProviders = this.providers.filter { it.id !in existingIds }
+
+       uiState = uiState.copy(providers = providers + disconnectedProviders)
     }
 }
 
@@ -34,7 +44,6 @@ sealed class AvailableProvider : Provider {
     data class Planned(override val id: DefinedProvider, override val isActive: Boolean = false) : AvailableProvider()
     data class Disconnected(
         override val id: DefinedProvider,
-        override val isActive: Boolean = true,
-        val onConnect: () -> Unit
+        override val isActive: Boolean = true
     ) : AvailableProvider()
 }
