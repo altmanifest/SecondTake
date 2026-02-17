5319d7bd0f56efd6e1c395341b4e69455247f9c4
Patryk Kowejsza <patryk@kowejsza.name>
2026-02-17 17:59:42 +0100

    feat: use ProviderSource to crreate provider selection buttons

--- app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/ui/screens/ProviderSelectionScreen.kt
+++ app/composeApp/src/commonMain/kotlin/com/altmanifest/secondtake/ui/screens/ProviderSelectionScreen.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.altmanifest.secondtake.ui.components.Header
 import com.altmanifest.secondtake.ui.components.ProviderButton
 import com.altmanifest.secondtake.ui.viewmodel.ProviderSelectionViewModel
@@ -21,7 +23,7 @@ fun ProviderSelectionScreen(
     modifier: Modifier = Modifier,
     viewModel: ProviderSelectionViewModel,
 ) {
-    val state = viewModel.uiState
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
 
     Column(
         modifier = modifier.padding(top = 20.dp),
